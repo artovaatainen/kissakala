2efc750d4ece155d12580737b4ea205854c451c6
artovaatainen <arto.vaatainen@gmail.com>
2016-09-01 20:14:50 +0300

    Add files via upload

---
+++ skriptit.js
@@ -0,0 +1,9 @@
+/*
+file: skriptit.js
+date: 20.2.2014
+desc: Hello World! esimerkki JavaScriptillä
+auth: AV
+*/
+function tulostaHeippa(){
+			document.getElementById('laatikko').innerHTML ='Heippa Maailma!';
+		}
